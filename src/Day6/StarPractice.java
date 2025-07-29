b4faf9e19f8cf14b16300becc48e46a4ef1d8b28
Seok_Hyeon <teai06112@gmail.com>
2025-07-29 18:53:19 +0900

    Day9

---
+++ src/Day6/StarPractice.java
@@ -0,0 +1,61 @@
+package Day6;
+
+public class StarPractice {
+    public static void main(String[] args) {
+
+        //*****
+        //*****
+        //*****
+        //*****
+//        //*****
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("*****");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+//        int lines = 5;
+//        for (int i = 1; i <= lines; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+
+        //*****
+        //****
+        //***
+        //**
+        //*
+//        int line = 5;
+//        for (int u = line; u >= 1; u--) { // 5부터 1까지 줄어드는 반복
+//            for (int j = 1; j <= line - u; j++) {
+//                System.out.print(" "); // 왼쪽 공백: 정렬 효과
+//            }
+//            for (int k = 1; k <= u; k++) {
+//                System.out.print("*"); // 현재 줄 번호만큼 별 출력
+//            }
+//            System.out.println(); // 줄 바꿈
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+        for (int i = 1; i <= 5; i++) {
+            // 왼쪽 공백 출력
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력: 2 * i - 1
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // 줄 바꿈
+        }
+    }
+}
+
+
+
