1436b102a9957294a95db5dfb83110f628b0baee
Seok_Hyeon <teai06112@gmail.com>
2025-08-11 12:46:22 +0900

    Day16

---
+++ src/Day12/TypeTest1.java
@@ -0,0 +1,18 @@
+package Day12;
+
+// 자료형 테스트
+public class TypeTest1 {
+    public static void main(String a[]) {
+        char ch = '\u0041';
+        String s = "abc\ndef\tghi";
+        byte b = 127;
+        b++;
+        int i = 0xff;
+        Long lo = 10000000000L;
+        System.out.println(ch);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(lo);
+    }
+}
